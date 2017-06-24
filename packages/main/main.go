8119c16330efe6ea10e11a36af61fee261747a2f
pmurphy881 <pmurphy881@gmail.com>
2017-06-24 13:55:41 -0400

    lesson 3 packages

---
+++ packages/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/pmurphy881/udemyTraining/packages/stringutil/"
+)
+
+func main() {
+    fmt.Println(stringutil.Reverse("!oG , olleH"))
+    fmt.Println(stringutil.MyName)
+}
